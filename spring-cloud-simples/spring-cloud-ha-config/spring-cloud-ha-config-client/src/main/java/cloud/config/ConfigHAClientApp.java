5434f26b8d51a0114b3e259fe9774fa6194cd745
wangjc1 <like_dart@126.com>
2018-06-05 13:47:39 +0800

    HA配置中心

--- spring-cloud-simples/spring-cloud-ha-config/spring-cloud-ha-config-client/src/main/java/cloud/config/ConfigHAClientApp.java
+++ spring-cloud-simples/spring-cloud-ha-config/spring-cloud-ha-config-client/src/main/java/cloud/config/ConfigHAClientApp.java
@@ -3,6 +3,8 @@ package cloud.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
  # 配置服务的servieId，即应用名。
  spring.cloud.config.discovery.serviceId=config-ha-server
 
+ 测试： http://localhost:8881/hi
  */
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
+@RefreshScope
 public class ConfigHAClientApp {
 
     public static void main(String[] args) {
