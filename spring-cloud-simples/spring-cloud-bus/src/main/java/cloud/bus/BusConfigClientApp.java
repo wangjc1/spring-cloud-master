37eb3f1f4e0ffd260bc24dec6159ebf1fb23cbad
wangjc1 <like_dart@126.com>
2018-06-05 20:32:38 +0800

    HA配置中心

--- spring-cloud-simples/spring-cloud-bus/src/main/java/cloud/bus/BusConfigClientApp.java
+++ spring-cloud-simples/spring-cloud-bus/src/main/java/cloud/bus/BusConfigClientApp.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
  访问http://localhost:8881/hi 或者http://localhost:8882/hi 浏览器显示：
  foo version 3
 
- 这时我们去代码仓库将foo的值改为“foo version 4”，即改变配置文件foo的值。如果是传统的做法，需要重启服务，才能达到配置文件的更新。此时，我们只需要发送post请求：http://localhost:8881/bus/refresh，你会发现config-client会重新读取配置文件
- 重新读取配置文件：
+ 这时我们去代码仓库将foo的值改为“foo version 4”，即改变配置文件foo的值。如果是传统的做法，需要重启服务，才能达到配置文件的更新。
+ 此时，我们只需要发送post请求：http://localhost:8881/bus/refresh
+ 你会发现config-client会重新读取配置文件
 
+ 重新读取配置文件：
  这时我们再访问http://localhost:8881/hi 或者http://localhost:8882/hi 浏览器显示：
  foo version 4
 
