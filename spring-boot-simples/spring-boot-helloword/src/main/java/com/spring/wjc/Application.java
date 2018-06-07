995a688599250b9027e13cfac02a701e9fe2500d
wangjc1 <like_dart@126.com>
2018-06-07 16:09:10 +0800

    配置加解密

--- spring-boot-simples/spring-boot-helloword/src/main/java/com/spring/wjc/Application.java
+++ spring-boot-simples/spring-boot-helloword/src/main/java/com/spring/wjc/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
  */
 @EnableAutoConfiguration
 //@Configuration
-//@ComponentScan
+//@ComponentScan(basePackages = "com.spring.wjc")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
