336e2ddcc0ae1e2b0c87344d8ff822bfdf2e7fad
wangjc1 <like_dart@126.com>
2018-06-25 10:55:06 +0800

    add kafka project

---
+++ spring-cloud-mq/spring-cloud-kafka/src/main/java/kafka/base/SimpleProducer.java
@@ -0,0 +1,30 @@
+package kafka.base;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+    private static final Logger Logger = LoggerFactory.getLogger(SimpleProducer.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        producer.send(new ProducerRecord<>("test", "luck", "luck dog"));
+        Logger.info("finish");
+        producer.close();
+    }
+}
